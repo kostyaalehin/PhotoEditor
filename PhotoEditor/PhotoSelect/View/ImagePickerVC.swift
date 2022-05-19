f58825540cd88a54d9fb0b5e0995aae246bb059f
kostyaalehin <koss595959@gmail.com>
2022-05-19 17:31:52 +0500

    add special view controller
    https://youtu.be/zGWehR1-8t8?t=2308

---
+++ PhotoEditor/PhotoSelect/View/ImagePickerVC.swift
@@ -0,0 +1,30 @@
+//
+//  ImagePickerVC.swift
+//  PhotoEditor
+//
+//  Created by Константин Алехин on 19.05.2022.
+//
+
+import UIKit
+
+class ImagePickerVC: UIViewController {
+
+    @IBOutlet weak var imageList: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
