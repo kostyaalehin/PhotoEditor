0253ddb2ae2e1991b8b1eca4790fe8705d592683
kostyaalehin <koss595959@gmail.com>
2022-05-20 15:15:56 +0500

    add ImageCell
    https://youtu.be/zGWehR1-8t8?t=2749

---
+++ PhotoEditor/PhotoSelect/View/Cell/ImageCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCell.swift
+//  PhotoEditor
+//
+//  Created by Константин Алехин on 20.05.2022.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
