18f3674b41befa34de57df3433fe85eac3f731e5
kostyaalehin <koss595959@gmail.com>
2022-05-23 22:37:13 +0500

    create ListOwner

---
+++ PhotoEditor/PhotoSelect/service/ListOwner.swift
@@ -0,0 +1,14 @@
+//
+//  ListOwner.swift
+//  PhotoEditor
+//
+//  Created by Константин Алехин on 23.05.2022.
+//
+
+import Foundation
+
+protocol ListOwner: AnyObject {
+    func selectImages (index: Int) {
+
+    }
+}
